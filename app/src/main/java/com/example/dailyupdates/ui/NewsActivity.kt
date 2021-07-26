babc3c648cde5a4d00035b707e0d0806b5e9f25d
PraveenAade15 <laxmipraveen101@gmail.com>
2021-07-26 20:50:49 +0530

    Gradle builds

--- app/src/main/java/com/example/dailyupdates/ui/NewsActivity.kt
+++ app/src/main/java/com/example/dailyupdates/ui/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.example.dailyupdates
+package com.example.dailyupdates.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dailyupdates.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
